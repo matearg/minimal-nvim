80979f0d41eb3c67115a8883f822572bb05be271
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2023-02-18 19:55:32 -0300

    Catppuccin removed

--- lua/mateo/packer.lua
+++ lua/mateo/packer.lua
@@ -24,8 +24,6 @@ return require('packer').startup(function(use)
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
-    -- use { "catppuccin/nvim", as = "catppuccin" }
-
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use {
         'lewis6991/gitsigns.nvim',
