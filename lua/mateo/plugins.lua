f656df3a1f42178704d6ab9973160c9d1a4edd9a
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2023-05-22 16:43:37 -0300

    Oil is nice

--- lua/mateo/plugins.lua
+++ lua/mateo/plugins.lua
@@ -70,6 +70,14 @@ return require('lazy').setup({
   },
 
   {
+    'stevearc/oil.nvim',
+    config = function ()
+      require('oil').setup({})
+      vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
+    end
+  },
+
+  {
     'akinsho/bufferline.nvim',
     config = function()
       require('bufferline').setup()
