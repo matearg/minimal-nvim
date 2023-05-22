53bc8a2f8d88acc523797453cff3a675a9cc1867
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2023-05-22 14:44:16 -0300

    Fixed colorscheme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -11,7 +11,7 @@ function ColorMyPencils(color)
     }
   })
 
-  color = color or "tokyonight"
+  color = color or "onedark"
   vim.cmd.colorscheme(color)
 
   -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
